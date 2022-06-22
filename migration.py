ff56d15d9f1c32c04a0e675cdefb07aff097ed71
kostik <kostikvlz@mail.ru>
2022-06-22 20:27:23 +0300

    added connection with pgsql

--- migration.py
+++ migration.py
@@ -1,28 +1,41 @@
-import sqlite3 as db
+import psycopg2
+from psycopg2 import Error
 
+try:
+    # подключаюсь к существующей БД
+    with psycopg2.connect(user="postgres",
+                          password="123",
+                          host="127.0.0.1",
+                          port="5432",
+                          database="postgres_db") as connection:
 
-with db.connect('base.db', check_same_thread=False) as connection:
-    cursor = connection.cursor()
+        cursor = connection.cursor()
 
-    # создаю таблицу в БД
-    cursor.execute("DROP TABLE IF EXISTS products")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS products (
-        type TEXT NOT NULL,
-        base_costs_per_month INTEGER DEFAULT NULL,
-        consumption_costs INTEGER DEFAULT NULL,
-        tariff_price INTEGER DEFAULT NULL,
-        tariff_consumption INTEGER DEFAULT NULL,
-        additional_costs INTEGER DEFAULT NULL
-    )""")
+        cursor.execute("DROP TABLE IF EXISTS products")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS products (
+                   type TEXT NOT NULL,
+                   base_costs_per_month INTEGER DEFAULT NULL,
+                   consumption_costs INTEGER DEFAULT NULL,
+                   tariff_price INTEGER DEFAULT NULL,
+                   tariff_consumption INTEGER DEFAULT NULL,
+                   additional_costs INTEGER DEFAULT NULL)""")
 
-    # вношу в таблицу три записи тарифа basic
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 4, 22)')
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 5, 25)')
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 3, 20)')
-    # вношу в таблицу три записи тарифа package
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 800, 4000, 30)""")
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 900, 4500, 23)""")
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 850, 5000, 27)""")
+        print("Таблица успешно создана в PostgreSQL")
+
+        # вношу в таблицу три записи тарифа basic
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 4, 22)")
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 5, 25)")
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 3, 20)")
+
+        # вношу в таблицу три записи тарифа package
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 800, 4000, 30)""")
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 900, 4500, 23)""")
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 850, 5000, 27)""")
+        connection.commit()
+        print('Изменения в таблицу успешно внесены')
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
