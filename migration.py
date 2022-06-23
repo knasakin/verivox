7f09a7fa2e9a6e34f8450fbae016148069cce31d
kostik <kostikvlz@mail.ru>
2022-06-23 11:52:05 +0300

    lil changes

--- migration.py
+++ migration.py
@@ -1,28 +1,42 @@
-import sqlite3 as db
+from psycopg2 import Error, connect
+from config import user, password, host, port, database
 
 
-with db.connect('base.db', check_same_thread=False) as connection:
-    cursor = connection.cursor()
+try:
+    # подключаюсь к существующей БД
+    with connect(user=user,
+                 password=password,
+                 host=host,
+                 port=port,
+                 database=database) as connection:
 
-    # создаю таблицу в БД
-    cursor.execute("DROP TABLE IF EXISTS products")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS products (
-        type TEXT NOT NULL,
-        base_costs_per_month INTEGER DEFAULT NULL,
-        consumption_costs INTEGER DEFAULT NULL,
-        tariff_price INTEGER DEFAULT NULL,
-        tariff_consumption INTEGER DEFAULT NULL,
-        additional_costs INTEGER DEFAULT NULL
-    )""")
+        cursor = connection.cursor()
 
-    # вношу в таблицу три записи тарифа basic
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 4, 22)')
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 5, 25)')
-    cursor.execute('INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ("basic", 3, 20)')
-    # вношу в таблицу три записи тарифа package
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 800, 4000, 30)""")
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 900, 4500, 23)""")
-    cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
-                      VALUES ("package", 850, 5000, 27)""")
+        cursor.execute("DROP TABLE IF EXISTS products")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS products (
+                   type TEXT NOT NULL,
+                   base_costs_per_month INTEGER DEFAULT NULL,
+                   consumption_costs INTEGER DEFAULT NULL,
+                   tariff_price INTEGER DEFAULT NULL,
+                   tariff_consumption INTEGER DEFAULT NULL,
+                   additional_costs INTEGER DEFAULT NULL)""")
+
+        print("Таблица успешно создана в PostgreSQL")
+
+        # вношу в таблицу три записи тарифа basic
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 4, 22)")
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 5, 25)")
+        cursor.execute("INSERT INTO products (type, base_costs_per_month, consumption_costs) VALUES ('basic', 3, 20)")
+
+        # вношу в таблицу три записи тарифа package
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 800, 4000, 30)""")
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 900, 4500, 23)""")
+        cursor.execute("""INSERT INTO products (type, tariff_price, tariff_consumption, additional_costs)
+                                         VALUES ('package', 850, 5000, 27)""")
+        connection.commit()
+        print('Изменения в таблицу успешно внесены')
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
